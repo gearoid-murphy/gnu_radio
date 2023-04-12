338873f955c1b573ae5f9dcbc842521dd0e188f5
Gearoid Murphy <gearoid.p.murphy@gmail.com>
2023-04-11 21:19:59 -0700

    Prepping for the m2402a.

--- hackrf_amplifier.py
+++ hackrf_amplifier.py
@@ -6,9 +6,9 @@
 #
 # GNU Radio Python Flow Graph
 # Title: HackRF Amplifier Control
-# GNU Radio version: 3.8.1.0
+# GNU Radio version: 3.10.1.1
 
-from distutils.version import StrictVersion
+from packaging.version import Version as StrictVersion
 
 if __name__ == '__main__':
     import ctypes
@@ -24,20 +24,25 @@ from PyQt5 import Qt
 from gnuradio import analog
 from gnuradio import gr
 from gnuradio.filter import firdes
+from gnuradio.fft import window
 import sys
 import signal
 from argparse import ArgumentParser
 from gnuradio.eng_arg import eng_float, intx
 from gnuradio import eng_notation
 from gnuradio.qtgui import Range, RangeWidget
+from PyQt5 import QtCore
 import osmosdr
 import time
+
+
+
 from gnuradio import qtgui
 
 class hackrf_amplifier(gr.top_block, Qt.QWidget):
 
     def __init__(self):
-        gr.top_block.__init__(self, "HackRF Amplifier Control")
+        gr.top_block.__init__(self, "HackRF Amplifier Control", catch_exceptions=True)
         Qt.QWidget.__init__(self)
         self.setWindowTitle("HackRF Amplifier Control")
         qtgui.util.check_set_qss()
@@ -73,24 +78,24 @@ class hackrf_amplifier(gr.top_block, Qt.QWidget):
         self.samp_rate = samp_rate = 32000
         self.rf_gain = rf_gain = 1
         self.if_gain = if_gain = 20
-        self.freq = freq = 107e6
+        self.freq = freq = 2.45e9
 
         ##################################################
         # Blocks
         ##################################################
-        _rf_gain_check_box = Qt.QCheckBox('rf_gain')
+        _rf_gain_check_box = Qt.QCheckBox("'rf_gain'")
         self._rf_gain_choices = {True: 14, False: 1}
         self._rf_gain_choices_inv = dict((v,k) for k,v in self._rf_gain_choices.items())
         self._rf_gain_callback = lambda i: Qt.QMetaObject.invokeMethod(_rf_gain_check_box, "setChecked", Qt.Q_ARG("bool", self._rf_gain_choices_inv[i]))
         self._rf_gain_callback(self.rf_gain)
         _rf_gain_check_box.stateChanged.connect(lambda i: self.set_rf_gain(self._rf_gain_choices[bool(i)]))
-        self.top_grid_layout.addWidget(_rf_gain_check_box)
+        self.top_layout.addWidget(_rf_gain_check_box)
         self._if_gain_range = Range(1, 47, 1, 20, 200)
-        self._if_gain_win = RangeWidget(self._if_gain_range, self.set_if_gain, 'if_gain', "counter_slider", float)
-        self.top_grid_layout.addWidget(self._if_gain_win)
-        self._freq_range = Range(100e6, 110e6, 1, 107e6, 200)
-        self._freq_win = RangeWidget(self._freq_range, self.set_freq, 'freq', "counter_slider", float)
-        self.top_grid_layout.addWidget(self._freq_win)
+        self._if_gain_win = RangeWidget(self._if_gain_range, self.set_if_gain, "'if_gain'", "counter_slider", float, QtCore.Qt.Horizontal)
+        self.top_layout.addWidget(self._if_gain_win)
+        self._freq_range = Range(2.4e9, 2.5e9, .1e9, 2.45e9, 200)
+        self._freq_win = RangeWidget(self._freq_range, self.set_freq, "'freq'", "counter_slider", float, QtCore.Qt.Horizontal)
+        self.top_layout.addWidget(self._freq_win)
         self.osmosdr_sink_0 = osmosdr.sink(
             args="numchan=" + str(1) + " " + ""
         )
@@ -106,15 +111,18 @@ class hackrf_amplifier(gr.top_block, Qt.QWidget):
         self.analog_const_source_x_0 = analog.sig_source_c(0, analog.GR_CONST_WAVE, 0, 0, 1)
 
 
-
         ##################################################
         # Connections
         ##################################################
         self.connect((self.analog_const_source_x_0, 0), (self.osmosdr_sink_0, 0))
 
+
     def closeEvent(self, event):
         self.settings = Qt.QSettings("GNU Radio", "hackrf_amplifier")
         self.settings.setValue("geometry", self.saveGeometry())
+        self.stop()
+        self.wait()
+
         event.accept()
 
     def get_samp_rate(self):
@@ -148,6 +156,7 @@ class hackrf_amplifier(gr.top_block, Qt.QWidget):
 
 
 
+
 def main(top_block_cls=hackrf_amplifier, options=None):
 
     if StrictVersion("4.5.0") <= StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
@@ -156,10 +165,15 @@ def main(top_block_cls=hackrf_amplifier, options=None):
     qapp = Qt.QApplication(sys.argv)
 
     tb = top_block_cls()
+
     tb.start()
+
     tb.show()
 
     def sig_handler(sig=None, frame=None):
+        tb.stop()
+        tb.wait()
+
         Qt.QApplication.quit()
 
     signal.signal(signal.SIGINT, sig_handler)
@@ -169,12 +183,7 @@ def main(top_block_cls=hackrf_amplifier, options=None):
     timer.start(500)
     timer.timeout.connect(lambda: None)
 
-    def quitting():
-        tb.stop()
-        tb.wait()
-    qapp.aboutToQuit.connect(quitting)
     qapp.exec_()
-
 
 if __name__ == '__main__':
     main()
